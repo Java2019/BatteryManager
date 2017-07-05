85ec2a3efd8249ca59796a4842f37cfaa006a410
java2019 <java_2019@mail.ru>
2017-07-05 17:14:57 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/hardware/batterymanager/BatteryManagerActivity.java
@@ -0,0 +1,13 @@
+package com.samples.hardware.batterymanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BatteryManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battery_manager);
+    }
+}
