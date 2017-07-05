61f9d5afbf2596380f4554cf9491fc1f4b64a10c
java2019 <java_2019@mail.ru>
2017-07-05 17:22:37 +0300

    test1

--- app/src/main/java/com/samples/hardware/batterymanager/BatteryManagerActivity.java
+++ app/src/main/java/com/samples/hardware/batterymanager/BatteryManagerActivity.java
@@ -2,12 +2,39 @@ package com.samples.hardware.batterymanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class BatteryManagerActivity extends AppCompatActivity {
+public class BatteryManagerActivity extends AppCompatActivity
+        implements View.OnClickListener{
+
+    private Button bStart;
+    private Button bStop;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battery_manager);
+
+        text = (TextView)findViewById(R.id.text);
+        bStart = (Button)findViewById(R.id.bStart);
+        bStop = (Button)findViewById(R.id.bStop);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bStart:
+
+                break;
+            case R.id.bStop:
+
+                break;
+        }
     }
 }
